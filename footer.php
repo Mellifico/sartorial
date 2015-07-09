ee333d4d3d05aa140ac4ba4d3fab4c26d22ce6f7
Mellifico <mellifico@gmail.com>
2015-07-09 12:30:21 +0200

    Work in progress

--- footer.php
+++ footer.php
@@ -3,13 +3,12 @@
 <div class="wrapper">
 <footer class="row">
 <div class="large-12 column">
-<h5><a class="outline" href="mailto:mellifico@gmail.com">courriel</a></h5>
-<!-- <svg width="3.2em" height="3.2em"><use xlink:href="#ruche"></use></svg>
-<svg width="3.2em" height="3.2em"><use xlink:href="#alveole"></use></svg> -->
+
 
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
 	<?php do_action('foundationPress_after_footer'); ?>
+	</div>
 </footer>
 </div>
 </div>
--- front-page.php
+++ front-page.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 <?php do_action('foundationPress_before_content'); ?>
 <div class="wrapper brand text-center">
-<h1><?php bloginfo( 'name' ); ?><br /><?php bloginfo( 'description' ); ?></h1>
-</div>
-<div class="contain-to-grid sticky">
-<?php get_template_part('parts/top-bar'); ?>
+<h1 class="uppercase ligatures"><?php bloginfo( 'name' ); ?><br /><?php bloginfo( 'description' ); ?></h1>
+<h2><small>Alpha Version 0.1</small></h2>
 </div>
 
 <?php do_action('foundationPress_after_content'); ?>
