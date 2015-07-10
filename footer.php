782248f73a9344e4afc164870c455c176da50002
Mellifico <mellifico@gmail.com>
2015-07-10 09:31:24 +0200

    Typographie

--- footer.php
+++ footer.php
@@ -2,13 +2,16 @@
 
 <div class="wrapper">
 <footer class="row">
-<div class="large-12 column">
-
-
+<div class="medium-6 large-4 columns">
+<h3 class="hyphen ligatures"><small><strong>à propos</strong><br />
+Cet espace a été conçu pour permettre à des maisons de qualité de présenter chacune 10 objets représentatifs de leur travail. Son accès est et sera réservé aux maisons sélectionnées par l’équipe de PG, selon un cahier des charges qualitatif très précis. Le texte présentant la maison est rédigé par l’équipe PG. Les objets présentés, quant à eux, sont laissés au libre choix des maisons intégrant ce guide. Chaque objet est présenté de manière descriptive uniquement et les prix ne sont pas indiqués. Il s’agit donc d’un espace payant mais accessible uniquement aux maisons correspondant aux critères de qualité et à l’esprit d’excellence du site.</small>
+</h3>
+</div>
+<div class="medium-6 large-4 columns"></div>
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
 	<?php do_action('foundationPress_after_footer'); ?>
-	</div>
+	
 </footer>
 </div>
 </div>
--- front-page.php
+++ front-page.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 <?php do_action('foundationPress_before_content'); ?>
 <div class="wrapper brand text-center">
-<h1 class="uppercase ligatures"><?php bloginfo( 'name' ); ?><br /><?php bloginfo( 'description' ); ?></h1>
-<h2><small>Alpha Version 0.1</small></h2>
+<h1 id="big" class="uppercase ligatures"><?php bloginfo( 'name' ); ?><br /><?php bloginfo( 'description' ); ?></h1>
 </div>
 
 <?php do_action('foundationPress_after_content'); ?>
